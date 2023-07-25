0f87bb253dce0adf79da3dc0c44c8e5e32732569
gabang2 <offbeat1020@naver.com>
2023-07-25 19:08:24 +0900

    test2

--- backend/cook/api.py
+++ backend/cook/api.py
@@ -11,6 +11,7 @@ class RecipeList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RecipeList2(APIView):
--- backend/cook/urls.py
+++ backend/cook/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from cook import api
 
 urlpatterns = [
-    path('recipe_list/', api.RecipeList.as_view(), name="IngredientList"),
+    path('recipe_list/', api.RecipeList.as_view(), name="RecipeList"),
 ]

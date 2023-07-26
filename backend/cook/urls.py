52a2e805f3c2a444b87880c15e0f9179c6f2ab28
gabang2 <offbeat1020@naver.com>
2023-07-26 21:17:40 +0900

    refactor : __pycache__, .idea폴더 삭제

--- backend/cook/urls.py
+++ backend/cook/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from cook import api
-from .views import RecipeListAPIView
 
 urlpatterns = [
-    path('recipes/', RecipeListAPIView.as_view(), name="recipe-list"),
     path("list/<int:num>/", api.CookList.as_view(), name="cook_list")
 ]

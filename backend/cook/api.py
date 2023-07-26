85474b2f738767b5e921f1e947d1465bc2b7ab05
gabang2 <offbeat1020@naver.com>
2023-07-26 17:21:59 +0900

    Feat : 재고 보유율 반영해서 음식 list추출하도록

--- backend/cook/api.py
+++ backend/cook/api.py
@@ -18,12 +18,32 @@ class CookList(APIView):
         ## 사용자 정보 ##
         user, _ = User.objects.get_or_create(user_number=request.META["HTTP_AUTHORIZATION"])
 
-        ## 사용자 냉장고 재고 ##
+        ## 냉장고 재고 ##
         user_ingredient = Refrigerator.objects.filter(user=user).values_list("ingredient", flat=True)
 
-        ## 사용자 냉장고 재고와 겹치는 요리들 ##
+        ## 냉장고 재고와 겹치는 요리들 일치율 순서로 정렬 ##
         user_cook = CookIngredient.objects.filter(ingredient__in=user_ingredient).values_list("cook", flat=True)
-        counter = Counter(list(user_cook))
-        print(counter)
+        user_cook_cnt = Counter(list(user_cook))
+        user_cook_cnt = sorted(user_cook_cnt.items(), key=lambda x: x[1], reverse=True)  # 겹치는 개수 순서로 정렬
+        user_cook_list = [u[0] for u in user_cook_cnt]
 
-        return Response({"message": "success"})
+        result = list()
+        ## 요청받은 수만큼 정렬
+        if len(user_cook_list) > num:
+            for i in range(num):
+                cook = Cook.objects.get(pk=user_cook_list[i])
+                result.append(
+                    {
+                        "id": cook.pk,
+                        "image_route": cook.image_route,
+                        "name": cook.name,
+                        "percent": 100
+                    }
+                )
+        else:
+            pass
+
+        response = Response()
+        response.data = result
+        response.status = status.HTTP_200_OK
+        return response

dc25d1c9a36c9c748ad6a42092bc1c498542b630
kyungjunleeme <kyungjunlee.me@gmail.com>
2023-07-26 17:52:55 +0900

    CHG: typo 수정

--- backend/cook/serializers.py
+++ backend/cook/serializers.py
@@ -4,21 +4,8 @@ from .models import Cook
 from .models import Ingredient
 from .models import Recipe
 
-# class TrackSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Track
-#         fields = ['order', 'title', 'duration']
-#
-# class AlbumSerializer(serializers.ModelSerializer):
-#     tracks = TrackSerializer(many=True, read_only=True)
-#
-#     class Meta:
-#         model = Album
-#         fields = ['album_name', 'artist', 'tracks']
 
-
-
-class RecipeSerilizer(serializers.ModelSerializer):
+class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = (
@@ -27,7 +14,8 @@ class RecipeSerilizer(serializers.ModelSerializer):
             'image_route',
         )
 
-class IngredientSerilizer(serializers.ModelSerializer):
+
+class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = (
@@ -35,10 +23,9 @@ class IngredientSerilizer(serializers.ModelSerializer):
         )
 
 
-
 class CookListSerializer(serializers.ModelSerializer):
-    cook_ingredient = IngredientSerilizer(read_only=True, many=True)
-    cook_recipe = RecipeSerilizer(read_only=True, many=True)
+    cook_ingredient = IngredientSerializer(read_only=True, many=True)
+    cook_recipe = RecipeSerializer(read_only=True, many=True)
 
     class Meta:
         model = Cook
@@ -50,11 +37,3 @@ class CookListSerializer(serializers.ModelSerializer):
             'cook_ingredient',
             'cook_recipe',
         ]
-
-
-    # name = models.TextField()
-    # method = models.CharField(max_length=20, null=True)
-    # image_route = models.TextField(null=True)
-    # ingredient = models.TextField(null=True)
-    # cook_ingredient = models.ManyToManyField(Ingredient, related_name="cooks", through="CookIngredient")
-    # cook_recipe

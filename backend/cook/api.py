49cf8d1beb655d8f360b8b97f83845a0b3b87501
gabang2 <offbeat1020@naver.com>
2023-07-26 16:26:28 +0900

    Feat : 사용자 재고와 비교해서 요리 목록 데이터 가져오기 완료

--- backend/cook/api.py
+++ backend/cook/api.py
@@ -1,27 +1,29 @@
+'''builtin'''
+from collections import Counter
+
+'''Third-Party'''
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
-# from cook.serializers import RecipeSerializer
-
-
-class RecipeList(APIView):
-    def post(self, request):
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''Django'''
+from user.models import User
+from refrigerator.models import Refrigerator
+from cook.models import Cook, CookIngredient
 
 
-class RecipeList2(APIView):
-    def post(self, request):
-        number = request.data["number"]
-        detail = request.data["detail"]
-        image_route = request.data["image_route"]
-
-
+# 사용자별 음식 추천 목록 보여주기
 class CookList(APIView):
-    def get(self, request):
+    def get(self, request, num):
+        ## 사용자 정보 ##
+        user, _ = User.objects.get_or_create(user_number=request.META["HTTP_AUTHORIZATION"])
+
+        ## 사용자 냉장고 재고 ##
+        user_ingredient = Refrigerator.objects.filter(user=user).values_list("ingredient", flat=True)
+
+        ## 사용자 냉장고 재고와 겹치는 요리들 ##
+        user_cook = CookIngredient.objects.filter(ingredient__in=user_ingredient).values_list("cook", flat=True)
+        counter = Counter(list(user_cook))
+        print(counter)
+
         return Response({"message": "success"})

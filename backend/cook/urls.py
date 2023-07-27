1dcd7bfc570c73df8b98d1b01d7e16f569a47a26
gabang2 <offbeat1020@naver.com>
2023-07-27 13:43:18 +0900

    Add : Ingredient list 출력 api

--- backend/cook/urls.py
+++ backend/cook/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from cook import views
 
 urlpatterns = [
-    path("list/<int:num>/", views.CookList.as_view(), name="cook_list")
+    path("list/<int:num>/", views.CookList.as_view(), name="cook_list"),  # 추천 요리 목록
+    path("ingredient/list/", views.IngredientList.as_view(), name="ingredient_list"),  # db전체 재료 목록
 ]
